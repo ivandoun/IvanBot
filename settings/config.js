12666b1ca512b9c6f81b40901280c8a508167581
ivandoun <89326484+ivandoun@users.noreply.github.com>
2021-08-21 23:21:13 -0300

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'IvanBot',
+        ownerName: 'Ivandoun',
+        // youtube: 'YOUTUBE_LINK',
+        instagram: 'INSTAGRAM_LINK',
+}
---
+++ src/version.js
@@ -0,0 +1,14 @@
+const version = (prefix, pushname) => {
+    return `
+*IvanBot by ivandoun*
+
+*Actualizado:* 18 de Agosto del 2021
+*Versión actual:* 3.7
+*Ofrecida por:* ivandoun
+
+ву ivandoun
+`
+
+}
+
+exports.version = version
